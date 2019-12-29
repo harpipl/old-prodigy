0f9d0ac4e089f2ecdee2acf7da667fdacecf542c
harpipl <arkadiusz.adamczak@gmail.com>
2019-12-29 11:01:01 +0100

    Removed security credentials

--- prodigy-impl/src/main/java/pl/harpi/prodigy/runtime/service/StructureService.java
+++ prodigy-impl/src/main/java/pl/harpi/prodigy/runtime/service/StructureService.java
@@ -1,8 +1,5 @@
 package pl.harpi.prodigy.runtime.service;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -18,13 +15,7 @@ import java.util.Map;
 public class StructureService {
     @Cacheable(value = "cacheProducts", key = "#productId")
     public Map<String, Object> getStructure(Long productId) {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                "AKIAJHRY4FUV63DHK5AA",
-                "3H4VZuM5A/IyQxYJfK9G253ZOZ36/RyWuKlAUqJK"
-        );
-
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(Regions.EU_CENTRAL_1).build();
         String str = s3.getObjectAsString("harpipl-prodigy", "structures/products/0000000000000000001.json");
 
